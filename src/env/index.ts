3ece3560fe3ec492478189b78d0a7997f88b3147
JJOptimist <optimistkum888@gmail.com>
2024-09-02 00:18:17 +0200

    revert to default

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,11 +1,2 @@
-declare global {
-  interface ImportMeta {
-	readonly env: {
-      VITE_APP_JOYID_APP_URL: string;
-	  readonly VITE_APP_CALLBACK_SERVER_URL: string;
-	};
-  }
-}
-
 export const CALLBACK_SERVER_URL = import.meta.env.VITE_APP_CALLBACK_SERVER_URL ?? "";
 export const JOYID_APP_URL = import.meta.env.VITE_APP_JOYID_APP_URL ?? "https://testnet.joyid.dev/";
